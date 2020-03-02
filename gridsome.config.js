3948ee88aa0e718170ea69b5744d453d42f391fa
Yashu Mittal <mittalyashu77@gmail.com>
2020-03-02 09:15:32 +0530

    update siteName, siteDescription, siteUrl and titleTemplate

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,8 @@
-// This is where project configuration and plugin options are located. 
-// Learn more: https://gridsome.org/docs/config
-
-// Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
-
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+	siteName: "LogChimp",
+	siteDescription: "Track user feedback to build better products",
+	siteUrl: "https://logchimp.codecarrot.net/",
+	titleTemplate: "%s | LogChimp",
+
+	plugins: []
 }
