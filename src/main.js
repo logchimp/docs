19581ff8eb4ced8ee0ef913db90d60244fcd41e0
Yashu Mittal <mittalyashu77@gmail.com>
2020-03-02 09:10:12 +0530

    Use base stylesheet
    install node-sass &  sass-loader package and adding few base styles.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-// This is the main.js file. Import global CSS and scripts here.
-// The Client API can be used here. Learn more: gridsome.org/docs/client-api
+// import styles globally
+import "~/assets/styles/main.sass"
 
 import DefaultLayout from '~/layouts/Default.vue'
 
