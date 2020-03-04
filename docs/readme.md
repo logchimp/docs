dbeb704307bb9a11b5ff1ccdc6eb7a978ae47880
Yashu Mittal <mittalyashu77@gmail.com>
2020-03-04 14:46:58 +0530

    docs layout (#1)
    * Introduction readme.md docs page
    
    * Install @gridsome/vue-remark@0.1.10 package
    
    * vue-remark plugin for docs pages using docs template layout
    
    * docs sidebar
    
    * Create separate section around docs__content
    
    * docs sidebar menu json data
    
    Pass docs sidebar menu data as props to docsSidebar component.
    
    * docs frontmatter (title + description)
    
    * Markdown styles
    
    * docs: logchimp intro & community information

---
+++ docs/readme.md
@@ -0,0 +1,15 @@
+---
+title: Introduction
+description: Quick start guide for using LogChimp.
+---
+
+[LogChimp](/) is open-source self-hostable platform built on a modern Node.js technology stack allows you to collect/track user feedback to build better products.
+
+## Community
+
+Stay up to date on the development of LogChimp and reach out to the community with these helpful resources.
+
+- Follow [@logchimp](https://twitter.com/@logchimp) on Twitter.
+- Keep track of [development on GitHub](https://github.com/logchimp/logchimp).
+- Checkout the [designs on Figma](https://www.figma.com/community/file/816586866419993972/LogChimp-Designs).
+- Join the official [LogChimp Discord server](https://discordapp.com/invite/A7mztcC/).
---
+++ src/templates/Docs.vue
@@ -0,0 +1,49 @@
+<template>
+	<div class="container">
+		<div class="docs">
+			<div class="docs__sidebar">
+				<docs-sidebar :menu="docsMenu" />
+			</div>
+			<div class="docs__content">
+				<div class="docs__meta">
+					<div class="docs__meta-heading">
+						{{ $page.doc.title }}
+					</div>
+				</div>
+				<div class="markdown">
+					<VueRemarkContent />
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+// data
+import DocsMenu from "../../data/docs.json";
+
+// components
+import DocsSidebar from "../components/DocsSidebar";
+
+export default {
+	name: "Docs",
+	components: {
+		DocsSidebar
+	},
+	computed: {
+		docsMenu() {
+			return DocsMenu;
+		}
+	}
+};
+</script>
+
+<page-query>
+query DocPage ($path: String!) {
+	doc: docPage (path: $path) {
+		title
+		path
+		content
+	}
+}
+</page-query>
