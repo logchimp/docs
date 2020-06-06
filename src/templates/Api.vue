c81ad7e7c0c7a30f7f1c9ec7bcde891afdc56bab
Yashu Mittal <mittalyashu77@gmail.com>
2020-06-06 07:56:48 +0530

    API template page

---
+++ src/templates/Api.vue
@@ -0,0 +1,39 @@
+<template>
+	<docs-layout
+		:menu="apiMenu"
+		:title="$page.api.title"
+		route="api"
+	>
+		<VueRemarkContent />
+	</docs-layout>
+</template>
+
+<script>
+// data
+import ApiMenu from "../../data/api.json";
+
+// components
+import DocsLayout from "../layouts/Docs";
+
+export default {
+	name: "Api",
+	components: {
+		DocsLayout
+	},
+	computed: {
+		apiMenu() {
+			return ApiMenu;
+		}
+	}
+};
+</script>
+
+<page-query>
+query ApiPage ($path: String!) {
+	api: apiPage (path: $path) {
+		title
+		path
+		content
+	}
+}
+</page-query>
