5c117f4ff78e73f4db7b2115eacb3abb8194db62
Yashu Mittal <mittalyashu77@gmail.com>
2020-06-15 15:14:21 +0530

    Header (#3)
    * Create logchimp logo vue component
    
    * Create PrimaryButton component
    
    * Header component
    
    Import header component to default layout vue component and remove old css styles and html code.
    
    * Header wrapper and mobile navbar
    
    Use header wrapper for container css class and hide navbar on mobile view.

---
+++ src/layouts/partials/Header.vue
@@ -0,0 +1,43 @@
+<template>
+	<header class="header">
+		<div class="container">
+			<div class="header__wrapper">
+				<div class="header__navbar">
+					<logo class="header__logo" />
+					<nav class="navbar">
+						<g-link to="/product" class="navbar__item">
+							Product
+						</g-link>
+						<g-link to="/docs" class="navbar__item">
+							Developers
+						</g-link>
+						<g-link to="/resources" class="navbar__item">
+							Resources
+						</g-link>
+					</nav>
+				</div>
+				<div class="header__action">
+					<primary-button>
+						Get started now
+					</primary-button>
+				</div>
+			</div>
+		</div>
+	</header>
+</template>
+
+<script>
+// components
+import PrimaryButton from "../../components/PrimaryButton";
+
+// icons
+import Logo from "../../assets/images/logo";
+
+export default {
+	name: "Header",
+	components: {
+		PrimaryButton,
+		Logo
+	}
+};
+</script>
