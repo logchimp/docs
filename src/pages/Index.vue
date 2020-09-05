9c4c69c6051c3ad47fd54c76a834b15b61aa631e
Yashu Mittal <mittalyashu77@gmail.com>
2020-09-05 09:37:01 +0530

    Add 'Coming Soon' to homepage

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,25 @@
 <template>
-  <Layout>
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
-  </Layout>
+	<Layout>
+		<div class="container">
+			<h1 class="comingsoon">Coming Soon...</h1>
+		</div>
+	</Layout>
 </template>
 
 <script>
 export default {
-  metaInfo: {
-    title: 'Hello, world!'
-  }
-}
+	title: "Homepage",
+	metaInfo: {
+		title: "Coming Soon",
+	},
+};
 </script>
 
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
+
+<style lang='sass' scoped>
+.comingsoon
+	margin-top: 4rem
+	font-size: 4rem
+	font-weight: 400
+	text-align: center
 </style>
