2b2b94becb212c9c0933edc15664fe29b5ecddc5
Yashu Mittal <mittalyashu77@gmail.com>
2020-09-05 10:14:22 +0530

    Improve DocsSidebar component styles

--- src/components/DocsSidebar.vue
+++ src/components/DocsSidebar.vue
@@ -1,15 +1,15 @@
 <template>
-	<div class="dsidebar">
-		<div v-for="section in menu" class="dsidebar__section">
-			<div class="dsidebar__title">
+	<div class="DocsSidebar">
+		<div v-for="section in menu" class="DocsSidebar__section">
+			<div class="DocsSidebar__section-title">
 				{{ section.section }}
 			</div>
-			<div class="dsidebar__list">
+			<div class="DocsSidebar__list">
 				<g-link
 					:to="`/${route}/${item.slug}`"
 					v-for="item in section.topics"
 					:key="`${route}-${item.name}`"
-					class="dsidebar__item"
+					class="DocsSidebar__item"
 				>
 					{{ item.name }}
 				</g-link>
