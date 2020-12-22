e996246da41b0249b05d652664a944199932488e
Yashu Mittal <mittalyashu77@gmail.com>
2020-12-22 11:49:28 +0530

    Remove API data source

--- gridsome.config.js
+++ gridsome.config.js
@@ -14,19 +14,6 @@ module.exports = {
 				template: "./src/templates/Docs.vue",
 				index: ["readme"]
 			}
-		},
-		{
-			use: "@gridsome/vue-remark",
-			options: {
-				typeName: "ApiPage",
-				baseDir: "./api",
-				pathPrefix: "/api",
-				template: "./src/templates/Api.vue",
-				index: ["readme"],
-				plugins: [
-					"@gridsome/remark-prismjs"
-				]
-			}
 		}
 	]
 }
