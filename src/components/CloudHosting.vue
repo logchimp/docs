4d8a5f68a260106e54f4ddd786218167d5ef6e5f
Yashu Mittal <mittalyashu77@gmail.com>
2021-02-06 09:47:16 +0530

    Create CloudHosting component

---
+++ src/components/CloudHosting.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="products">
+		<g-link
+			v-for="product in products"
+			:key="product.title"
+			class="product"
+			:to="product.link"
+		>
+			<img :src="product.image" :alt="product.title" />
+			<h3>{{ product.title }}</h3>
+			<p>{{ product.label }}</p>
+		</g-link>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "CloudHosting",
+	data() {
+		return {
+			products: [
+				{
+					title: "Render",
+					image: "/images/docs/render-logo.png",
+					link: "/docs/install/render",
+					label: "One click deploy"
+				}
+			]
+		}
+	}
+}
+</script>
