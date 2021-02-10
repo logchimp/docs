ef5b89742c1b90bcffceb22535ff791d80685d8e
Yashu Mittal <mittalyashu77@gmail.com>
2021-02-10 11:09:14 +0530

    Create logo component

---
+++ src/components/Logo.vue
@@ -0,0 +1,12 @@
+<template>
+	<g-link to="/" class="logo">
+		<img src="https://cdn.logchimp.codecarrot.net/logchimp_circular_logo.png" alt="LogChimp logo">
+		<h6>LogChimp</h6>
+	</g-link>
+</template>
+
+<script>
+export default {
+	name: "Logo"
+}
+</script>
