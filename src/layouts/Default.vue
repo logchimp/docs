76227cda9ea6ac17af3d1ed4d4713d3715280f7a
Yashu Mittal <mittalyashu77@gmail.com>
2021-02-10 11:13:31 +0530

    Add footer component to Default layout

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,18 +1,21 @@
 <template>
-	<div>
+	<div class="inner">
 		<Header />
 		<slot />
+		<Footer />
 	</div>
 </template>
 
 <script>
 // components
 import Header from "../layouts/partials/Header";
+import Footer from "../layouts/partials/SmallFooter";
 
 export default {
 	name: "Default",
 	components: {
-		Header
+		Header,
+		Footer
 	}
 }
 </script>
