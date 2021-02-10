5f24903da2480d73876a0a5b12508cf5db6527ee
Yashu Mittal <mittalyashu77@gmail.com>
2021-02-10 17:03:02 +0530

    Use static layout styles in partners page

--- src/pages/Partners.vue
+++ src/pages/Partners.vue
@@ -1,30 +1,32 @@
 <template>
 	<Layout>
-		<div class="container">
-			<div class="partners">
-				<div class="partners-header">
-					<h3>LogChimp partners</h3>
-					<p>
-						As a Non-Profit Open Source company we are incredibly grateful to
-						companies who provide services to support our ongoing work.
-					</p>
-				</div>
+		<div class="static-layout">
+			<header class="static-layout-header">
+				<h2 class="static-layout-heading">Partners</h2>
+				<p class="small-text">
+					As a Non-Profit open-source organization, we are incredibly grateful
+					to companies who provide services to support our ongoing work.
+				</p>
+			</header>
 
-				<hr />
-
-				<div class="partners-list">
-					<div
-						:key="partner.id"
-						v-for="partner in partners"
-						class="partners-list-item"
+			<div class="partners-list">
+				<div
+					:key="partner.id"
+					v-for="partner in partners"
+					class="partners-list-item"
+				>
+					<a
+						:href="`${partner.link}?utm_source=website&utm_medium=referral&company=logchimp`"
 					>
-						<a :href="`${partner.link}?utm_source=website&utm_medium=referral&company=logchimp`">
-							<img :src="partner.image" :alt="partner.title" :title="partner.title" />
-						</a>
-						<p>
-							{{ partner.description }}
-						</p>
-					</div>
+						<img
+							:src="partner.image"
+							:alt="partner.title"
+							:title="partner.title"
+						/>
+					</a>
+					<p>
+						{{ partner.description }}
+					</p>
 				</div>
 			</div>
 		</div>
