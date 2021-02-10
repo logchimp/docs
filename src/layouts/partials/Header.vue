5273dc4b705ce5942c4aee15318e2f76163c1146
Yashu Mittal <mittalyashu77@gmail.com>
2021-02-10 12:55:41 +0530

    remove nav links & action button from header

--- src/layouts/partials/Header.vue
+++ src/layouts/partials/Header.vue
@@ -1,24 +1,10 @@
 <template>
 	<header>
-		<div class="inner">
-			<div class="header__wrapper">
-				<div class="header__navbar">
-					<g-link class="header__logo" to="/">
-						<img src="@/assets/images/logo_with_name.svg" alt="LogChimp logo">
-					</g-link>
-					<nav class="navbar">
-						<!-- <g-link to="/product" class="navbar__item">
-							Product
-						</g-link> -->
-						<g-link to="/docs/" class="navbar__item"> Developers </g-link>
-						<!-- <g-link to="/resources" class="navbar__item">
-							Resources
-						</g-link> -->
-					</nav>
-				</div>
-				<div class="header__action">
-					<Button type="primary"> Get started now </Button>
-				</div>
+		<div class="header__wrapper">
+			<div class="header__navbar">
+				<logo />
+			</div>
+			<div class="header__action">
 			</div>
 		</div>
 	</header>
@@ -26,12 +12,12 @@
 
 <script>
 // components
-import Button from "../../components/Button";
+import Logo from "../../components/Logo";
 
 export default {
 	name: "Header",
 	components: {
-		Button,
+		Logo
 	},
 };
 </script>
