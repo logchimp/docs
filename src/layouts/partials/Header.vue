b93fa841267833bff57b18736c0f3abfc73f75cd
Yashu Mittal <mittalyashu77@gmail.com>
2021-02-10 14:44:48 +0530

    Add outline button to header link to github repository

--- src/layouts/partials/Header.vue
+++ src/layouts/partials/Header.vue
@@ -5,6 +5,12 @@
 				<logo />
 			</div>
 			<div class="header__action">
+				<Button @click="githubRepository" type="outline">
+					<template #icon>
+						<github-icon width="1.5rem" height="1.5rem" fill="#1A1A1A" />
+					</template>
+					GitHub
+				</Button>
 			</div>
 		</div>
 	</header>
@@ -13,11 +19,24 @@
 <script>
 // components
 import Logo from "../../components/Logo";
+import Button from "../../components/Button";
+
+// icons
+import GithubIcon from "../../components/icons/github";
 
 export default {
 	name: "Header",
 	components: {
-		Logo
+		Logo,
+		Button,
+
+		// icons
+		GithubIcon
 	},
+	methods: {
+		githubRepository() {
+			window.location.href = "https://github.com/logchimp/logchimp"
+		}
+	}
 };
 </script>
