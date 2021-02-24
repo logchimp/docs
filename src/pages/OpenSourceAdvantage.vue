f5a3c6c1099b849962f7ad25974890259a2592b1
Yashu Mittal <mittalyashu77@gmail.com>
2021-02-25 00:42:38 +0530

    create page open-source advantages

---
+++ src/pages/OpenSourceAdvantage.vue
@@ -0,0 +1,98 @@
+<template>
+	<Layout>
+		<div class="container">
+			<header class="static-layout-header">
+				<h2 class="static-layout-heading">Why open source?</h2>
+				<p class="small-text">
+					LogChimp was/is/will be open-source for you to collect feedback from
+					your customers 😉
+				</p>
+			</header>
+
+			<div class="open-source-list">
+				<div class="open-source-item">
+					<img
+						src="@/assets/images/logchimp-github-repo.jpg"
+						alt="LogChimp Github repository"
+					/>
+					<div class="open-source-item-content">
+						<h2>Open source</h2>
+						<p>
+							An open source software means your team retains control over the
+							service’s technical roadmap and your team is free to develop
+							additional features without restriction. Supported by a vibrant
+							open source community.
+						</p>
+					</div>
+				</div>
+
+				<div class="open-source-item">
+					<img
+						src="@/assets/images/logchimp-github-repo.jpg"
+						alt="LogChimp Github repository"
+					/>
+					<div class="open-source-item-content">
+						<h2>Data ownership</h2>
+						<p>
+							
+						</p>
+					</div>
+				</div>
+
+				<div class="open-source-item">
+					<img
+						src="@/assets/images/logchimp-github-repo.jpg"
+						alt="LogChimp Github repository"
+					/>
+					<div class="open-source-item-content">
+						<h2>Security</h2>
+						<p>
+							Run LogChimp site under your existing security and IT policies.
+							Deploy to public, private or hybrid clouds with full access to
+							source code and total control of your single-tenant system.
+						</p>
+					</div>
+				</div>
+
+				<div class="open-source-item">
+					<img
+						src="@/assets/images/logchimp-github-repo.jpg"
+						alt="LogChimp Github repository"
+					/>
+					<div class="open-source-item-content">
+						<h2>Privacy</h2>
+						<p>
+							Your IP address, your usage patterns, the contents of your
+							messages—SaaS services know more about your company than you do.
+							In contrast, Mattermost provides the benefits of modern
+							communication without sacrificing privacy.
+						</p>
+					</div>
+				</div>
+
+				<div class="open-source-item">
+					<img
+						src="@/assets/images/logchimp-github-repo.jpg"
+						alt="LogChimp Github repository"
+					/>
+					<div class="open-source-item-content">
+						<h2>Scalability</h2>
+						<p>
+							Mattermost was built ground-up for enterprise with a single-tenant
+							private cloud architecture offering high availability and
+							horizontal scaling. Tens of thousands of users can operate out of
+							a single team and hundreds of teams can be deployed in an
+							enterprise.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</Layout>
+</template>
+
+<script>
+export default {
+	name: "OpenSourceAdvantagePage"
+}
+</script>
