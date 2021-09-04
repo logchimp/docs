0366c48945c973a4cf5690d84abc59df19e0e5bb
Yashu Mittal <mittalyashu77@gmail.com>
2021-09-04 09:17:09 +0530

    update "Copyright" message in footer

--- src/layouts/partials/SmallFooter.vue
+++ src/layouts/partials/SmallFooter.vue
@@ -6,7 +6,7 @@
 				<div class="footer-col">
 					<logo />
 					<p class="small-text">
-						© Copyright {{ new Date().getFullYear() }} CodeCarrot
+						Copyright © {{ new Date().getFullYear() }} CodeCarrot. All rights reserved.
 					</p>
 				</div>
 
