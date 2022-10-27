da08a350018ff3c0c36960e30f3d4febd89c80eb
mittalyashu <mittalyashu77@gmail.com>
2022-10-27 10:55:20 +0000

    feat: configure vite as prerender: true

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import ssr from 'vite-plugin-ssr/plugin'
 import { UserConfig } from 'vite'
 
 const config: UserConfig = {
-  plugins: [vue(), ssr()]
+  plugins: [vue(), ssr({ prerender: true })]
 }
 
 export default config
