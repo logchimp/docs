ba9a06ad49cb14dba24668c4b7304a1be4a69bed
Ja4V8s28Ck <66721411+Ja4V8s28Ck@users.noreply.github.com>
2025-09-21 22:56:36 +0530

    feat: show method on the sidebar based on the API call & lint

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -1,33 +1,37 @@
 import { createElement } from "react";
 import { loader } from "fumadocs-core/source";
+import { transformerOpenAPI } from "fumadocs-openapi/server";
 import { icons } from "lucide-react";
 import { apiReference, docs, guide } from "@/.source";
 
 export const docsSource = loader({
-  source: docs.toFumadocsSource(),
-  baseUrl: "/docs",
-  icon(icon) {
-    if (!icon) {
-      return;
-    }
+	source: docs.toFumadocsSource(),
+	baseUrl: "/docs",
+	icon(icon) {
+		if (!icon) {
+			return;
+		}
 
-    if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
-  },
+		if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
+	},
 });
 
 export const guideSource = loader({
-  source: guide.toFumadocsSource(),
-  baseUrl: "/guide",
-  icon(icon) {
-    if (!icon) {
-      return;
-    }
+	source: guide.toFumadocsSource(),
+	baseUrl: "/guide",
+	icon(icon) {
+		if (!icon) {
+			return;
+		}
 
-    if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
-  },
+		if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
+	},
 });
 
 export const openApiSource = loader({
-  source: apiReference.toFumadocsSource(),
-  baseUrl: "/api-reference",
+	source: apiReference.toFumadocsSource(),
+	baseUrl: "/api-reference",
+	pageTree: {
+		transformers: [transformerOpenAPI()],
+	},
 });
