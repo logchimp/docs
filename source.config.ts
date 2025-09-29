a64ce66731ab51f13a2e77ebb8c03ea49e714661
Ja4V8s28Ck <66721411+Ja4V8s28Ck@users.noreply.github.com>
2025-09-29 12:56:39 +0530

    searches all page (#365)

--- source.config.ts
+++ source.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig, defineDocs } from "fumadocs-mdx/config";
 
+export const main = defineDocs({
+  dir: "content/",
+});
+
 export const docs = defineDocs({
   dir: "content/docs",
 });
--- src/lib/source.ts
+++ src/lib/source.ts
@@ -2,7 +2,19 @@ import { createElement } from "react";
 import { loader } from "fumadocs-core/source";
 import { transformerOpenAPI } from "fumadocs-openapi/server";
 import { icons } from "lucide-react";
-import { apiReference, docs, guide } from "@/.source";
+import { apiReference, docs, guide, main } from "@/.source";
+
+export const mainSource = loader({
+  source: main.toFumadocsSource(),
+  baseUrl: "/",
+  icon(icon) {
+    if (!icon) {
+      return;
+    }
+
+    if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
+  },
+});
 
 export const docsSource = loader({
   source: docs.toFumadocsSource(),
