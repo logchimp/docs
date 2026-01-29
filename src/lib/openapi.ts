078bd7611d627f5f35fa5b3306809d77b02d18b9
Ja4V8s28Ck <66721411+Ja4V8s28Ck@users.noreply.github.com>
2026-01-29 22:06:52 +0530

    refactor: move openapi.yaml to content/
    - fixes recursion error

--- src/lib/openapi.ts
+++ src/lib/openapi.ts
@@ -1,11 +1,5 @@
 import { createOpenAPI } from "fumadocs-openapi/server";
 
 export const openapi = createOpenAPI({
-  input: ["./content/api-reference/openapi.yaml"],
-  shikiOptions: {
-    themes: {
-      dark: "vesper",
-      light: "vitesse-light",
-    },
-  },
+  input: ["./content/openapi.yaml"],
 });
