b5da7ddd1e474ae8d3f6b32d6e4d6d135abc2a15
Yashu Mittal <mittalyashu77@gmail.com>
2026-01-30 18:20:49 +0530

    format the code

--- src/components/ApiPage.ts
+++ src/components/ApiPage.ts
@@ -2,10 +2,10 @@ import { openapi } from "../lib/openapi";
 import { createAPIPage } from "fumadocs-openapi/ui";
 
 export const APIPage = createAPIPage(openapi, {
-	shikiOptions: {
-		themes: {
-			dark: "vesper",
-			light: "vitesse-light",
-		},
-	},
+  shikiOptions: {
+    themes: {
+      dark: "vesper",
+      light: "vitesse-light",
+    },
+  },
 });
